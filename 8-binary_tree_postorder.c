ae9eea60d35c0b0aab58b3f457ccd5ae719f7448
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-01-31 08:56:25 -0800

    modifiy

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -9,10 +9,10 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-        if (tree && func)
-        {
-            binary_tree_postorder(tree->left, func);
-            binary_tree_postorder(tree->right, func);
-            func(tree->n);
-        }
+if (tree && func)
+{
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
+func(tree->n);
+}
 }
