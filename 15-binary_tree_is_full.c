fb51c17895c78f0b637f9cac1ea5dc92ae50926b
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-02-01 14:58:15 -0800

    Done

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -19,4 +19,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 
 	return (1);
+
 }
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -47,6 +47,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		{
 			return (1);
 		}
+
 		return (0);
 	}
 }
